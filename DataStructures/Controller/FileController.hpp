340f6457e9ed6754f145bde3ca1aa3f2c339233a
Sleepy <hillsteadm@gmail.com>
2019-02-07 08:34:14 -0700

    fixed some data files and made a list header

---
+++ DataStructures/Controller/FileController.hpp
@@ -0,0 +1,31 @@
+//
+//  FileController.hpp
+//  DataStructures
+//
+//  Created by Hillstead, Matthew on 2/5/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef FileController_hpp
+#define FileController_hpp
+
+#include "../Data/CrimeData.hpp"
+#include "../Data/Music.hpp"
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+class FileController
+{
+public:
+    static vector<CrimeData> readCrimeDataToVector(string filename);
+    
+    static vector<Music> musicDataToVector(string filename);
+};
+
+
+#endif /* FileController_hpp */
